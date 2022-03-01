65ab1c5caf008be491fdf5f608f87b38d3faafe6
akshay3054 <98379260+akshay3054@users.noreply.github.com>
2022-03-01 14:23:45 +0530

    Update main.js

--- main.js
+++ main.js
@@ -89,6 +89,27 @@ let standard = {
 }
 
 
+let budget = {
+  board : 1850,
+  laminate : 0,
+  hinges : 250,
+  channels : 250,
+  handles : 200,
+  fevicol : 50,
+  edgeBindingTape : 15,
+  screw : 150,
+  lock : 200,
+  packingMaterialCost : 0,
+  additionalCarpenterPayments : 800,
+  helper : 400,
+  inboundLogistics : 500,
+  outboundLogistics : 0,
+  labour : 1500,
+  salesInstallation : 1000,
+  partnerMaterialMargin : 0,
+  ucComissionLabour : 8,
+  ucComissionMaterial : 8
+}
 
 function calculate(){
 
@@ -253,6 +274,41 @@ function calculate(){
     ucEarnings =
     ((labourCost + logisticCost + partnerEarning)*standard.ucComissionLabour)/100 +
     (materialCost * standard.ucComissionMaterial)/100;
+
+    finalCost = Math.round(materialCost + labourCost + logisticCost + partnerEarning + ucEarnings);
+
+    document.getElementById('Standard').innerText = (Math.round(finalCost/100)*100).toLocaleString('en-IN');
+  
+  // Calculating Budget
+
+    materialCost =
+    boards*budget.board +
+    channels * budget.channels +
+    laminate*budget.laminate +
+    hinges*budget.hinges +
+    handles * budget.handles +
+    fevicol * budget.fevicol +
+    edgeBindingTape * budget.edgeBindingTape +
+    screw * budget.screw +
+    locks * budget.lock+
+    packingMaterialCost * budget.packingMaterialCost ;
+
+    labourCost =
+    additionalCarpenterPayments * budget.additionalCarpenterPayments +
+    helper * budget.helper * 0.75;
+
+    logisticCost =
+    inboundLogistics * budget.inboundLogistics +
+    outboundLogistics * budget.outboundLogistics;
+
+    partnerEarning =
+    labour * budget.labour * 0.75 +
+    salesInstallation * budget.salesInstallation +
+    (materialCost*budget.partnerMaterialMargin)/100;
+
+    ucEarnings =
+    ((labourCost + logisticCost + partnerEarning)*budget.ucComissionLabour)/100 +
+    (materialCost * budget.ucComissionMaterial)/100;
 
     finalCost = Math.round(materialCost + labourCost + logisticCost + partnerEarning + ucEarnings);
 
